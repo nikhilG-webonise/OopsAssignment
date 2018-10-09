d6d54345ea8961fea5d7566395362a909a6ac1a0
nikhilG-webonise <43873662+nikhilG-webonise@users.noreply.github.com>
2018-10-09 18:16:50 +0530

    Update Brand.java

--- src/Brand.java
+++ src/Brand.java
@@ -4,9 +4,9 @@ public class Brand{
 	Scanner sc=new Scanner(System.in);
 	void Android_brand()
 	{
-		System.out.println("enter Your Brand choice number:");
 		System.out.println("1)Samsung");
 		System.out.println("2)MI");
+		System.out.println("enter Your Brand choice number:");
 		int Brand_choice=sc.nextInt();
 		switch (Brand_choice) {
 		case 1:	Samsung s=new Samsung();
