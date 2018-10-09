214efad4c555c062cc5ea41bae12e89304712630
nikhilG-webonise <43873662+nikhilG-webonise@users.noreply.github.com>
2018-10-09 18:25:46 +0530

    Update MI.java

--- src/MI.java
+++ src/MI.java
@@ -47,7 +47,7 @@ public class MI extends Common_features{
 				default:System.out.println("You've Enter incorrect choice");
 						break;
 			}
-			System.out.println("Do you want to continue 'y' or 'n'?");
+			System.out.println("Do you want to continue 'y' or do you want to Select another OS 'n'?");
 			ch=sc.next().charAt(0);
 		}while(ch!='n');
 		Mobile mb=new Mobile();
