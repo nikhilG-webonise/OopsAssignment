02a9fd68999bafbe67bd6446eab100503f3823ff
nikhilG-webonise <nikhi.gaikwad@weboniselab.com>
2018-10-19 12:35:24 +0530

    Deleting  from master

--- UpdatedAssignment/src/common_feature.java
+++
@@ -1,6 +0,0 @@
-public interface common_feature {
-	  void message(String mobile_number,String message) ;
-	  void message(String mobile_number,String message,String attachment);
-	  void Make_cal(String mobile_number);
-	  void Take_photo();
-}
