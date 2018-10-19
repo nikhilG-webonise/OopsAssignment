a809308566a9558e48a3a9fa7944aa6ff8cc937c
nikhilG-webonise <nikhi.gaikwad@weboniselab.com>
2018-10-19 12:12:09 +0530

    Updated Assignment

---
+++ UpdatedAssignment/src/MiPhone.java
@@ -0,0 +1,10 @@
+
+public class MiPhone extends AndroidOSPhone{
+	
+	void google_Pay() {
+		System.out.println("Youre using google play");
+	}
+	void faceRecognisation() {
+		System.out.println("Youre using face Recognisation");
+	}
+}
