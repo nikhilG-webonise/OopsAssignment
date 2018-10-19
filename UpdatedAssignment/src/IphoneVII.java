a72a3cfe9e8009ce0de12f1ab8280b771cc00701
nikhilG-webonise <nikhi.gaikwad@weboniselab.com>
2018-10-19 12:40:01 +0530

    Updated Assignment

---
+++ UpdatedAssignment/src/IphoneVII.java
@@ -0,0 +1,11 @@
+
+public class IphoneVII extends IosOSPhone{
+	
+	void apple_Pay() {
+		System.out.println("youre using apple pay");
+	}
+	
+	void fingerPrintScanner() {
+		System.out.println("Youre Using security system fingerprint");
+	}
+}
